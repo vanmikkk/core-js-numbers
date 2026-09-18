e271e4604ad8fc73b4ffffc4ee1d8340e764a598
vanmik <ivap2006@gmail.com>
2026-09-18 15:05:17 +0500

    feat: add 15 function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -280,8 +280,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = n; i > 0; i -= 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**
