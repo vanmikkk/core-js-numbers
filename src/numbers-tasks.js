1a60aeb27c7039e1195fe1f5d972e7ef57bacc77
vanmik <ivap2006@gmail.com>
2026-09-18 15:09:17 +0500

    feat: add 15 function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -299,8 +299,9 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const nums = String(num).split('');
+  return nums.reduce((sum, value) => sum + Number(value), 0);
 }
 
 /**
