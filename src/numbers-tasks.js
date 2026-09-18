680f1542fcbb7bd81637d60f6e37917477cc59f0
vanmik <ivap2006@gmail.com>
2026-09-18 15:40:21 +0500

    feat: add 36 function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -628,8 +628,8 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  return Math.ceil(Math.abs(number) / 2);
 }
 
 module.exports = {
