680bdbc3bc976b62606414c29a259fb469444337
vanmik <ivap2006@gmail.com>
2026-09-18 15:27:32 +0500

    feat: add 16 function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -315,8 +315,15 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  if (num < 1) {
+    return false;
+  }
+  let value = num;
+  while (value % 2 === 0) {
+    value /= 2;
+  }
+  return value === 1;
 }
 
 /**
